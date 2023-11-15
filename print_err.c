9a6f56a08fae3b7680c33f0f20c2e3587f291aa6
SIHAM <siham@YoshiMario>
2023-11-15 19:51:51 +0100

    authors file

--- print_err.c
+++ print_err.c
@@ -12,14 +12,14 @@ char *_atoi(int num)
 	char digits[] = "0123456789";
 	char buffer[50];
 	char *ptr = &buffer[49];
-
+	char* output;
 	*ptr = '\0';
 	do {
 		*--ptr = digits[num % 10];
 		num /= 10;
 	} while (num);
-
-	return (_strdup(ptr));
+    output=_strdup(ptr);
+	return (output);
 }
 
 /**
@@ -42,5 +42,6 @@ void print_err(char *name, char *command, int counter)
 	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, command, _strlen(command));
 	write(STDERR_FILENO, ": not found\n", 12);
+
 	free(a_counter);
 }
